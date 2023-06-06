877a673e709c2e30b4a515ac88715b087187f536
Gaurav237 <gktiwary237@gmail.com>
2023-06-06 18:10:56 +0530

    added & styled html form & added create-task router for adding new task

--- index.js
+++ index.js
@@ -10,11 +10,34 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// form data parser
+app.use(express.urlencoded({ extended: true }));
+
+// add static files directory
+app.use(express.static('assets'));
+
 app.get('/', function(req, res){
     const data = {
         title: 'My Todo List'
     }
     return res.render('home', data);
+});
+
+// router for adding new task via form
+app.post('/create-task', function(req, res){
+    Todo.create({
+        description: req.body.description,
+        category: req.body.category,
+        dueDate: req.body.dueDate
+    })
+    .then(newTask => {
+        console.log('New Task Added : ', newTask);
+        return res.redirect('back');
+    })
+    .catch(err => {
+        console.log('error in adding the task : ', err);
+        return res.redirect('back');
+    })
 });
 
 app.listen(port, function(err){
